ce7d14aa1249563fd38f074dafcf4244fa08038e
CargoElf <benjamin.m.vansickle@gmail.com>
2024-07-08 18:27:29 -0500

    save point

--- src/components/CharacterSheet.js
+++ src/components/CharacterSheet.js
@@ -3,6 +3,8 @@ import { useState, useRef } from "react";
 
 function CharacterSheet() {
   const [ charName, setCharName ] = useState("");
+  const [ charAge, setCharAge ] = useState(18);
+  const [ charProfession, setCharProfession ] = useState("");
   const [ charAttrs, setCharAttrs ] = useState({
     str: 3,
     con: 3,
@@ -12,6 +14,8 @@ function CharacterSheet() {
     cha: 3
   });
   const [ isEditingName, setIsEditingName ] = useState(true);
+  const [ isEditingAge, setIsEditingAge ] = useState(false);
+  const [ isEditingProfession, setIsEditingProfession ] = useState(false);
 
   // TODO: move these to their own const file
   const strDescriptors = [
@@ -76,6 +80,13 @@ function CharacterSheet() {
     setCharName(nameText)
   };
 
+  const updateAge = (e) => {
+    const age = e.target.value
+    setCharAge(age)
+  }
+
+  const updateProfession = (e) => setCharProfession(e.target.value);
+
   const nameDisplay = () => {
     return (
       <div>
@@ -88,6 +99,7 @@ function CharacterSheet() {
                 size={charName.length || 1}
                 defaultValue={charName}
                 onChange={(e) => updateName(e)}
+                onFocus={() => setIsEditingName(true)}
                 onBlur={() => setIsEditingName(false)}
               />
               {isEditingName && <span className="blinking-block">▉</span>}
@@ -98,6 +110,47 @@ function CharacterSheet() {
     )
   }
 
+  const ageDisplay = () => {
+    return (
+      <div>
+        <span onClick={() => { if (!isEditingAge) setIsEditingAge(true) }}>
+          AGE: {
+            <span>
+              <input
+                size={charAge.toString().length}
+                defaultValue={charAge}
+                onChange={(e) => updateAge(e)}
+                onFocus={() => setIsEditingAge(true)}
+                onBlur={() => setIsEditingAge(false)}
+              />
+              {isEditingAge && <span className="blinking-block">▉</span>}
+            </span>
+          }
+        </span>
+      </div>
+    )
+  }
+
+  const professionDisplay = () => {
+    return (
+      <div>
+        <span onClick={() => { if (!isEditingProfession) setIsEditingProfession(true) }}>
+          Profession: {
+            <span>
+              <input
+                size={charProfession.length || 1}
+                defaultValue={charProfession}
+                onChange={(e) => updateProfession(e)}
+                onFocus={() => setIsEditingProfession(true)}
+                onBlur={() => setIsEditingProfession(false)}
+              />
+              {isEditingProfession && <span className="blinking-block">▉</span>}
+            </span>
+          }
+        </span>
+      </div>
+    )
+  }
   const availablePoints = () => {
     const valuesSum = Object.values(charAttrs).reduce(
       (total, value) => total + value, 0
@@ -174,14 +227,19 @@ function CharacterSheet() {
 |____/|_____|_____|_/_/   \_\  \____|_| \_\_____|_____|_| \_|`}
         </div>
         <div>
-          {String.raw` ___               _     ___              _         _
-| . |___ ___._ _ _| |_  |_ ____ _ _._ _ _<_._ _ ___| |
-|   / . / ._| ' | | |    | / ._| '_| ' ' | | ' <_> | |
-|_|_\_. \___|_|_| |_|    |_\___|_| |_|_|_|_|_|_<___|_|
-    <___'`}
+          {String.raw`
+ _____             _      _____              _         _
+|  _  |___ ___ ___| |_   |_   ____ ___ _____|_|___ ___| |
+|     | . | -_|   |  _|    | || -_|  _|     | |   | .'| |
+|__|__|_  |___|_|_|_|      |_||___|_| |_|_|_|_|_|_|__,|_|
+      |___|
+
+`}
         </div>
       </div>
       {nameDisplay()}
+      {ageDisplay()}
+      {professionDisplay()}
       {displayStats()}
       {derivedAttrs()}
     </div>
